8b1902a87a4a3b36dab0d67ebab8420d36162379
KD131 <cph-kd131@cphbusiness.dk>
2022-05-20 13:21:38 +0200

    fix isSpam call

--- server.py
+++ server.py
@@ -25,8 +25,8 @@ def webhook():
         most_recent_history_id = res['historyId']
         if(messages):
             for msg in messages:
-                if not gmail.isSpam(msg):
-                    subject, body = msg
+                subject, body = msg
+                if not gmail.isSpam(body):
                     print("subject:", subject)
                     print("body:", body)
                     try:
