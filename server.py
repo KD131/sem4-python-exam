a9a249fd9d3252b86848d9088a107dcd5809df24
Capa2 <johan931@gmail.com>
2022-05-20 11:28:26 +0200

    disabled shitty name exctractor

--- server.py
+++ server.py
@@ -34,10 +34,11 @@ def webhook():
                     if len(times) == 0: 
                         print("No datetime found.")
                         raise("No datetime found.")
-                    names = extract_names(body)
+                    #names = extract_names(body)
                     network_response = {
                         'title': subject,
-                        'description':"Persons: " + names + ". " + body,
+                        #'description':"Persons: " + names + ". " + body,
+                        'description':body,
                         'tag': label,  # social/business
                         'timeMin': times[0],
                         'timeMax': times[1]
@@ -47,9 +48,9 @@ def webhook():
                     writeToFile(label+body + " - event created: " + str(success))
                     return 'success', 200
                 except Exception as e:
-                    print('Insufficient data to build event', e)
-                    writeToFile('Insufficient data to build event'+body)
-                    return 'Insufficient data to build event',500
+                    print('Insufficient data to build event.', e)
+                    writeToFile('Insufficient data to build event.'+body)
+                    return 'Insufficient data to build event.',500
         else:
             return'no msg',200
     else:
