d90057423d8bb4a5279deae16cd4870e6708e6c8
Johan Karpantschof <johan931@gmail.com>
2022-05-24 10:22:32 +0200

    no print

--- datetime_extractor.py
+++ datetime_extractor.py
@@ -127,7 +127,7 @@ def parse_sets(sets):
     dts = []
     for set in sets:
         dt_string = set[1] + ' at ' + set[0]
-        print("dt_string", dt_string)
+        #print("dt_string", dt_string)
         time_struct, parse_status = cal.parse(dt_string)
         if parse_status:
             dt = datetime(
