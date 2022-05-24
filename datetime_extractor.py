28d1758c0b9d4a1d53f1d4aa4a0717fe88977d76
Johan Karpantschof <johan931@gmail.com>
2022-05-24 10:08:46 +0200

    tt

--- datetime_extractor.py
+++ datetime_extractor.py
@@ -160,7 +160,7 @@ def parse_items(items, pairs):
                 time_split[1] = str(9)
                 time_nine = "".join(time_split)
                 #print("join", date + "T" + time_nine)
-                dt = date + "DT" + time_nine
+                dt = date + "T" + time_nine
                 dts.append(dt)
     return dts
 
@@ -213,7 +213,11 @@ def extract_datetime(text):
 if __name__ == '__main__':
     #text = "There is a party at the 11th of july 10:00. It ends at 16:00. Also party at 22/01/23 at 14:00 and at aug 13 00:00"
     #text = "If you would come by my office after work, let's say Tuesday at 17:00, we can discuss that business matter you brought up earlier. We should be done by 19:00"
-    text = '''My name is tuesday'''
+    text = '''Here at Financial Holdings, we value integrity, robustness, reliability, and other such vague corporate buzzwords. We want you to come in for a scheduled meeting the 27/05 at 12:00 and you're not leaving until 27/05 14:00. I trust we can come to a satisfactory conclusion, at least for us as it might end in your termination.
+
+We'll be seeing you. Always.
+
+Beatrice Meagan, Financial Holdings HR Department'''
     # text = "Hej Johan."
     datetime = extract_datetime(text)
     print(text)
